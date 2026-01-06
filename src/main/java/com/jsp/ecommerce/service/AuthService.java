d8d301fa0394a28effcc860640586933cf3222f9
PunithRaj-N <punithrajn78@gmail.com>
2026-01-06 12:11:24 +0530

    Jwt security completed

---
+++ src/main/java/com/jsp/ecommerce/service/AuthService.java
@@ -0,0 +1,12 @@
+
+package com.jsp.ecommerce.service;
+
+import java.util.Map;
+
+import com.jsp.ecommerce.dto.LoginDto;
+
+public interface AuthService {
+
+	Map<String, Object> login(LoginDto loginDto);
+
+}
