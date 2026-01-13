376779ddba14e20b56b43db0a8282448951f5301
PunithRaj-N <punithrajn78@gmail.com>
2026-01-13 21:55:13 +0530

    merchant and customer resend otp and registration

--- src/main/java/com/jsp/ecommerce/controller/AdminController.java
+++ src/main/java/com/jsp/ecommerce/controller/AdminController.java
@@ -1,17 +1,34 @@
+
 package com.jsp.ecommerce.controller;
 
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jsp.ecommerce.service.AdminService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
-//	@GetMapping("/check")
-//	@PreAuthorize("hasRole('ADMIN')")
-//	public String check() {
-//		return "Working !!!";
-//	}
+	private final AdminService adminService;
+
+	@GetMapping("/merchants")
+	@PreAuthorize("hasRole('ADMIN')")
+	public Map<String, Object> viewMerchants() {
+		return adminService.getAllMerchants();
+	}
+
+	@GetMapping("/customers")
+	@PreAuthorize("hasRole('ADMIN')")
+	public Map<String, Object> viewCustomers() {
+		return adminService.getAllCustomers();
+	}
+
 }
